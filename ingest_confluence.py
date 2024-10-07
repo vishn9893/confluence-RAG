07e1da7555a29e6f96a3da916c0f8bed39035342
Vishnudhat Natarajan <Vishnudhat.Natarajan@ge.com>
2024-10-07 11:15:20 +0200

    Modify the loader

--- ingest_confluence.py
+++ ingest_confluence.py
@@ -16,8 +16,9 @@ load_dotenv()
 truststore.inject_into_ssl()
 
 CONFLUENCE_TOKEN = os.getenv('CONFLUENCE_TOKEN')
+CONFLUENCE_USERNAME = os.getenv('CONFLUENCE_USERNAME')
 CONFLUENCE_BASE_URL = os.getenv('CONFLUENCE_BASE_URL')
-CONFLUENCE_SPACES_TO_LOAD = os.getenv('CONFLUENCE_SPACES_TO_LOAD')
+# CONFLUENCE_SPACES_TO_LOAD = os.getenv('CONFLUENCE_SPACES_TO_LOAD')
 CONFLUENCE_MAX_PAGES_PER_SPACE = int(os.getenv('CONFLUENCE_MAX_PAGES_PER_SPACE', '10'))
 
 CONFLUENCE_CHUNK_SIZE = int(os.getenv('CONFLUENCE_CHUNK_SIZE', '1000'))
@@ -26,6 +27,9 @@ CONFLUENCE_CHUNK_OVERLAP = int(os.getenv('CONFLUENCE_CHUNK_OVERLAP', '100'))
 CONFLUENCE_RETRIES = int(os.getenv('CONFLUENCE_RETRIES', '10'))
 CONFLUENCE_MIN_RETRY_S = int(os.getenv('CONFLUENCE_MIN_RETRY_S', '2'))
 CONFLUENCE_MAX_RETRY_S = int(os.getenv('CONFLUENCE_MAX_RETRY_S', '10'))
+SPACE_KEY = os.getenv('CONFLUENCE_SPACE_KEY')
+
+print(CONFLUENCE_TOKEN)
 
 def main():
     # embeddings
@@ -38,23 +42,23 @@ def main():
     # vectorstore
     db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
 
-    logging.info(f"Reading spaces:{CONFLUENCE_SPACES_TO_LOAD} from {CONFLUENCE_BASE_URL}, limit {CONFLUENCE_MAX_PAGES_PER_SPACE} pages/space")
-    loader = ConfluenceLoader(url=CONFLUENCE_BASE_URL, token=CONFLUENCE_TOKEN, number_of_retries=CONFLUENCE_RETRIES, min_retry_seconds=CONFLUENCE_MIN_RETRY_S, max_retry_seconds=CONFLUENCE_MAX_RETRY_S)
+    # logging.info(f"Reading spaces:{CONFLUENCE_SPACES_TO_LOAD} from {CONFLUENCE_BASE_URL}, limit {CONFLUENCE_MAX_PAGES_PER_SPACE} pages/space")
+    loader = ConfluenceLoader(url=CONFLUENCE_BASE_URL, api_key=CONFLUENCE_TOKEN, username=CONFLUENCE_USERNAME, number_of_retries=CONFLUENCE_RETRIES, min_retry_seconds=CONFLUENCE_MIN_RETRY_S, max_retry_seconds=CONFLUENCE_MAX_RETRY_S)
 
-    for space in CONFLUENCE_SPACES_TO_LOAD:
-        # Process one space at a time
-        logging.info(f"Loading space {space}...")
-        documents = loader.load(space_key=space, content_format=ContentFormat.VIEW, max_pages=CONFLUENCE_MAX_PAGES_PER_SPACE)
-        logging.info(f"Loaded {len(documents)} documents")
-        texts = text_splitter.split_documents(documents)
-        logging.info(f"Split into {len(texts)} chunks of text")
+    # for space in CONFLUENCE_SPACES_TO_LOAD:
+    #     # Process one space at a time
+    #     logging.info(f"Loading space {space}...")
+    documents = loader.load(space_key=SPACE_KEY, content_format=ContentFormat.VIEW, max_pages=CONFLUENCE_MAX_PAGES_PER_SPACE)
+    logging.info(f"Loaded {len(documents)} documents")
+    texts = text_splitter.split_documents(documents)
+    logging.info(f"Split into {len(texts)} chunks of text")
 
-        # Save space embeddings
-        db.add_documents(texts)
-        db.persist()
-        
-        texts = None
-        documents = None
+    # Save space embeddings
+    db.add_documents(texts)
+    db.persist()
+    
+    texts = None
+    documents = None
 
 
 if __name__ == "__main__":
